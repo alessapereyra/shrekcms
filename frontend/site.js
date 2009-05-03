7c51a2db7cddd176ab0228918e919cf15953bf19
yaraher <alvaro.pereyra@srdperu.com>
2009-05-03 16:06:17 -0500

    Corrected some XHTML markup

--- frontend/site.js
+++ frontend/site.js
@@ -1,36 +1,69 @@
 $(document).ready(function() {
 	
 	
-		$("ul#menu > li a").click(function(){
+		$("ul#menu > li > a").click(function(){
 			
-			//alert();
+						alert("hi");	
 			
-			father = $(this).parent();
+						father = $(this).parent();
 
-			//Hides all the siblings
-			father.siblings().toggle();
+						//Hides all the siblings
+						father.siblings().toggle("fast");
 			
-			//Shows all the childs "li"
-			childs = father.find("li");
-
-			childs.toggle();
+						//Shows all the childs "li"
+						childs = father.find("li");
+						childs.toggle("fast");
 		
-			//Setups get back content
-			//TODO: I hate how this works
-			if ($(this)[0].textContent != "<"){
+						//Setups get back content
+						//TODO: I hate how this works
+						if ($(this)[0].textContent != "<"){
 				
-				$(this)[0].textContent = "<";
+							$(this)[0].textContent = "<";
 				
-			}
-			else {
+						}
+						else {
 				
-				$(this)[0].textContent = childs[0].innerHTML;
+							$(this)[0].textContent = childs[0].innerHTML;
 				
-			}
+						}
 		
 			
 		});
 		
+		
+			$("ul#menu li ul > li a").click(function(){
+
+				//alert();
+
+					father = $(this).parent();
+
+					//Hides all the siblings
+					//Shows all the childs "li"
+					childs = father.find("li");
+					alert ( childs.length );
+
+						if (childs.length > 1 ) { 
+
+						father.siblings().toggle("fast");
+
+						childs.toggle("fast");
+
+						//Setups get back content
+						//TODO: I hate how this works
+						if ($(this)[0].textContent != "<"){
+
+							$(this)[0].textContent = "<";
+
+						}
+						else {
+
+							$(this)[0].textContent = childs[0].innerHTML;
+
+						}
+					
+					}
+
+			});
 	
 	
 });
