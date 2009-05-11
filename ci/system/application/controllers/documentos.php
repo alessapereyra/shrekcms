371b2e330c445711e4a63ad8adc9d4f646b60420
Juan Alberto Farias <dientuki@grimlock.(none)>
2009-05-11 12:09:09 -0500

    subidon de documentos

---
+++ ci/system/application/controllers/documentos.php
@@ -0,0 +1,246 @@
+<?php
+
+class Documentos extends DI_Controller {
+	
+	function formulario($id = NULL)
+	{			
+		$data['id'] = NULL;
+		$data['titulo'] = NULL;
+		$data['texto'] = NULL;
+		$data['tags'] = NULL;
+		$data['doclink'] = NULL;		
+		$data['categorias'] = $this->_categorias();	
+		
+		$data['provincias'] = array('lima' => 'lima', 'callao' => 'callao');
+		$data['distritos'] = $data['provincias'];		
+		$data['departamentos'] = $data['provincias'];		
+		$data['paices'] = $data['provincias'];				
+		
+		if ($id != NULL)
+		{
+			$data = $this->_show($id, $data);
+		}
+		$data['form'] = $this->form;
+		
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->view('documentos/documento', $data);
+		$this->__destruct();		
+	}
+
+	function _show($id, $data)
+	{
+		$this->load->model('zones');
+		$consulta = $this->zones->seleccionar(array('id' => $id));
+		if ($consulta->num_rows() > 0)
+		{
+		   $fila = $consulta->row();
+		
+		   $data['id'] = $fila->id;
+		   $data['zona'] = $fila->zona;
+		}
+		return $data;		
+	}
+
+	function _categorias()
+	{
+		$this->load->model('terms');
+		return $this->terms->get_categories();	
+	}
+		
+	function actualizar()
+	{
+		$this->load->helper('url');
+		$this->load->helper('form');
+
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules($this->_reglas());
+		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+		 
+		if ($this->form_validation->run() == FALSE)
+		{
+			$data['id'] = $this->input->post('id');
+			$data['titulo'] = set_value('titulo');
+			$data['texto'] = set_value('texto');
+			$data['tags'] = set_value('tags');
+			$data['categorias'] = $this->_categorias();					
+
+			$this->load->view('documentos/documento', $data);
+			$this->__destruct();		
+
+		}
+		else
+		{
+			
+			$this->load->model('post');
+			$this->load->model('postmeta');
+			$this->load->model('terms');
+			$this->load->model('term_relationships');
+			$this->load->model('term_taxonomy');
+			
+			$id = $this->input->post('id');
+			$data['post_title']  = $this->input->post('titulo');
+			
+			$data['post_content'] = '';	
+	
+			switch ($this->input->post('upload-content'))
+			{
+				//subir imagenes
+				case 'subir':
+					$docs_id = split('-', $this->input->post('files'));
+					unset($docs_id[0]);
+					
+					foreach($docs_id as $doc)
+					{
+						$doc_data = $this->post->seleccionar(array('ID' => $doc));
+						$doc_data = $doc_data->row();
+						
+						$search_metadata['post_id'] = $doc;
+						$search_metadata['meta_key'] = '_wp_attached_file';
+						
+						$doc_name = $this->postmeta->seleccionar($search_metadata);
+						$doc_name = $doc_name->row_array();
+						$doc_name = split('/', $doc_name['meta_value']);
+						$doc_name = $doc_name[count($doc_name)-1];
+						
+						$tmp = '<a href="' . $doc_data->guid . '">';
+						$tmp .= $this->input->post('titulo');
+						$tmp .= '</a>';
+						$tmp .= '<br />';
+						$data['post_content'] .= $tmp;
+
+					}	
+					
+				break;
+				
+				//enlazar
+				case 'enlazar': 
+					$tmp = '<a href="' . $this->input->post('doclink') . '">';
+					$tmp .= $this->input->post('titulo');
+					$tmp .= '</a>';
+					$tmp .= '<br />';
+					$data['post_content'] .= $tmp;
+					
+				break;
+			}
+			
+			$data['post_content'] = $data['post_content'] . 'descripcion';
+			
+			//Debo armar el texto con las img
+			$data['tags'] = $this->input->post('tags');
+			
+			//consigue los id de las cata
+			$categorias = $this->_categorias();
+			
+			$terms_taxonomy_id = NULL;
+			
+			foreach($categorias as $key => $value)
+			{
+				if ($this->input->post('' . $key . ''))
+				{
+					$terms_taxonomy_id[] = $key;
+				}
+			}
+			
+			$tmp = array('provincia', 'distrito', 'pais', 'departamento');
+			
+			foreach ($tmp as $custom)
+			{
+				
+				if ($this->input->post($custom) != NULL)
+				{
+					$customs[$custom] = $this->input->post($custom);
+				}	
+			}
+
+			$data['terms_taxonomy_id'] = $terms_taxonomy_id;
+			
+			if ($id == NULL)
+			{
+				$time = $this->post->insert_article($data, $customs);
+			}
+			else
+			{
+				$where['id'] = $id;
+				$this->post->actualizar($data, $where);
+			}
+
+			if ($this->is_ajax != TRUE)
+			{
+				redirect('documentos/formulario');
+			}
+			else
+			{
+				$data['accion'] = 'ok';
+				$this->load->view('', $data);
+			}
+		}			
+	}
+	
+	function _reglas()
+	{
+		$reglas[] = array('field'   => 'titulo', 'label'   => 'lang:field_titulo', 'rules'   => 'trim|required|max_length[100]');
+		
+		return $reglas;
+	}
+
+	function ajax($accion)
+	{
+		switch ($accion)
+		{
+			case 'upload':
+				$this->_upload();
+			break;
+		}
+	}
+	
+	function _upload()
+	{
+		$tmp['allowed_types'] = 'doc|pdf';
+		$tmp['encrypt_name'] = TRUE;
+		
+		$this->load->model('options');
+		
+		$tmp['upload_path'] = $this->options->get_('upload_path') . date('/Y/m/');
+		$values['guid'] = $this->options->get_('upload_url') . date('/Y/m/');
+
+		$this->load->library('upload', $tmp);
+		
+		if ( ! $this->upload->do_upload('Filedata'))
+		{
+			$error = array('error' => $this->upload->display_errors(),
+							'upload_data' => $this->upload->data());
+			
+		}	
+		else
+		{			
+			$doc = $this->upload->data();
+
+			//debe insertar en un post, la imagen, ver wp_post id=18
+			$this->load->model('post');
+			$this->load->model('postmeta');
+			$this->load->helper('inflector');
+			
+			$values['post_author'] = $this->input->post('id');
+			$values['post_title'] = score(ereg_replace($doc['file_ext'], '' , $doc['file_name']));
+			$values['post_name'] = $values['post_title']; 
+			$values['post_mime_type'] = 'application/' . ereg_replace('\.', '' , $doc['file_ext']);
+			$values['guid'] = $values['guid'] . $doc['file_name'];
+			
+			$the_doc = $this->post->insert_attach($values);
+			
+			$meta['_wp_attached_file'] = date('Y/m/') . $doc['file_name'];
+			$meta['_wp_attachment_metadata'] = 'a:0{}';
+						
+			$this->postmeta->insertar($meta, $the_doc);
+			
+			echo $the_doc;
+		}
+		
+	}
+	
+}
+
+/* End of file monedas.php */
+/* Location: ./system/application/controllers/backend/monedas.php */
---
+++ ci/system/application/views/documentos/documento.php
@@ -0,0 +1,93 @@
+<div id="content" class="foto">
+
+	<?php echo form_open('documentos/actualizar', array('class' => 'form'));
+	
+		if ($id != NULL)
+		{
+			echo form_hidden('id', $id);					
+		}
+	?>
+	
+	<div id="text_content">
+  	<fieldset>
+
+  	<?php echo form_label('Titulo:', 'titulo');?>
+  	<?php echo form_error('titulo'); ?>
+  	<?php echo form_input(array('name' => 'titulo', 'value' => $titulo, 'id' => 'titulo')); ?>
+
+  	<div id="upload-content">
+  		<?php echo form_hidden('upload-content', 'subir'); ?>
+  		<ul>
+  			<li><a href="#subir">Subir</a></li>
+  			<li><a href="#enlazar">Enlazar</a></li>
+  		</ul>
+  		<div id="subir">
+  					<input type="hidden" id="files" name="files" value="" />
+  					<p>Selecciona el documento que desees subir:</p>
+  					<span id="spanButtonPlaceholder"></span>
+  					<div class="fieldset flash" id="fsUploadProgress"></div>
+  					<em>máximo 2mb. formatos soportados: doc, pdf</em>
+  		</div>
+  		<div id="enlazar">
+            	<?php echo form_label('Ingresa la dirección del documento que desees enviar:', 'doclink');?> 
+            	<?php echo form_error('doclink'); ?>
+            	<?php echo form_input(array('name' => 'doclink', 'value' => $doclink, 'id' => 'doclink')); ?>
+  		</div>		
+  	</div>
+
+  	<?php echo form_label('Descripci&oacute;n:', 'textos');?>
+  	<?php echo form_error('textos'); ?>
+  	<?php echo form_textarea(array('name' => 'textos', 'value' => $texto, 'id' => 'textos')); ?>
+
+  	<?php echo form_label('Etiquetas (separadas por comas):', 'tags');?> 
+  	<?php echo form_error('tags'); ?>
+  	<?php echo form_input(array('name' => 'tags', 'value' => $tags, 'id' => 'tags')); ?>
+  	<em>violencia, robos, denuncias, serenazgo, etc</em>
+  	
+  	</fieldset>
+  	
+    <div id="end_form">
+  	  <?php echo form_submit(array('class' => 'boton', 'name' => 'mysubmit', 'value' => 'Enviar' )); ?>    
+    </div>
+
+  	
+  		</div> <!-- text_content -->
+	
+	  <div id="sidebar_content">
+	   
+	  <h3>Categorizar</h3>
+  	<fieldset id="categories">
+  	<?php foreach($categorias as $key => $value): ?> 
+  	    <span>
+  		    <?php echo form_label($value . ':', $key);?> <?php echo form_checkbox(array('name' => $key, 'value' => TRUE, 'id' => $key)); ?>
+  		 </span>
+  	<?php endforeach; ?>	
+  	</fieldset>
+	
+  	<fieldset id="localizar">
+  	  
+  	  <h3>Localizar</h3>
+  		<?php echo form_hidden('localizar', 'peru'); ?>
+  		<ul>
+  			<li><a href="#peru">Perú</a></li>
+  			<li><a href="#mundo">El mundo</a></li>
+  		</ul>
+  		<div id="peru">
+  			<?php echo form_label('Provincia: ', 'provincia');?>
+  			<?php echo form_dropdown('provincia', $provincias, NULL,'id="provincia"'); ?>
+  			<?php echo form_label('Departamento: ', 'departamento');?>
+  			<?php echo form_dropdown('departamento', $departamentos, NULL,'id="departamento"'); ?>
+  			<?php echo form_label('Distrito: ', 'distrito');?>
+  			<?php echo form_dropdown('distrito', $distritos, NULL,'id="distrito"'); ?>
+  		</div>
+  		<div id="mundo">
+  			<?php echo form_label('País: ', 'pais');?>
+  			<?php echo form_dropdown('pais', $paices, NULL,'id="pais"'); ?>
+  		</div>	
+  	</fieldset>	
+
+	  </div> <!-- sidebar_content -->
+	
+	<?php echo form_close(); ?>
+
+</div>
