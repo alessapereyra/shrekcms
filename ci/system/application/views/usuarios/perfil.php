4fff33de0c6a877dcd9f0d711ed432edb659bbba
Juan Alberto Farias <dientuki@grimlock.(none)>
2009-05-17 18:37:35 -0500

    antes de push

--- ci/system/application/views/usuarios/perfil.php
+++ ci/system/application/views/usuarios/perfil.php
@@ -1,15 +1,7 @@
 
-<h2><?php echo print_r($nombre); ?></h2>
+<h2><?php echo $nombre; ?></h2>
 
-<h3><?php echo print_r($nickname); ?></h3>
+<h3><?php echo $nickname; ?></h3>
 <h4>Blogger de <a href="http://<?php echo print_r($blogs); ?>"><?php echo print_r($blogs); ?></a></h4>  
   
 <p><?php echo print_r($descripcion); ?></p>
-  
-  
-  
-<?php
-
-echo print_r($perfil);
-
-?>
