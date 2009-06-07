54ed4025af4eeb312559982f2c4e0a73f1e9f7d5
Felipe Guillermo Pereyra Nolasco <guillermopereyra@guillermopereyra-desktop.(none)>
2009-06-07 15:15:09 -0500

    html de geomula url listas

--- wp/wp-content/themes/lamula/geomula.php
+++ wp/wp-content/themes/lamula/geomula.php
@@ -1,3 +1,4 @@
+<?php $ci_url= "/shrekcms/mimula/"; ?>
 	<div id="menu_bar">
 
       <p id="tagline"><span class="geomule"><em>geo</em>mula</span></p>  <!-- tagline -->
@@ -9,12 +10,12 @@
               <li class="s"><a rel="callao" href="<?php echo '/shrekcms/mimula/ws/geomula/0/callao/callao/' ?>">Callao</a>
 
               <ul>
-              	<li class="t"><a rel="callao/bellavista" href="/shrekcms/mimula/ws/geomula/0/callao/callao/bellavista">Bellavista</a></li> 
-				<li class="t"><a rel="callao/carmen-de-la-legua" href="/shrekcms/mimula/ws/geomula/0/callao/callao/carmen-de-la-legua">Carmen de la Legua</a></li> 
-				<li class="t"><a rel="callao/cercado-callao" href="/shrekcms/mimula/ws/geomula/0/callao/callao/cercado-callao">Cercado Callao</a></li> 
-				<li class="t"><a rel="callao/la-perla" href="/shrekcms/mimula/ws/geomula/0/callao/callao/la-perla">La Perla</a></li> 
-				<li class="t"><a rel="callao/la-punta" href="/shrekcms/mimula/ws/geomula/0/callao/callao/la-punta">La Punta</a></li> 
-				<li class="t"><a rel="callao/ventanilla" href="/shrekcms/mimula/ws/geomula/0/callao/callao/ventanilla">Ventanilla</a></li> 
+              	<li class="t"><a rel="callao/bellavista" href="<?php echo $ci_url; ?>ws/geomula/0/callao/callao/bellavista">Bellavista</a></li> 
+				<li class="t"><a rel="callao/carmen-de-la-legua" href="<?php echo $ci_url; ?>ws/geomula/0/callao/callao/carmen-de-la-legua">Carmen de la Legua</a></li> 
+				<li class="t"><a rel="callao/cercado-callao" href="<?php echo $ci_url; ?>ws/geomula/0/callao/callao/cercado-callao">Cercado Callao</a></li> 
+				<li class="t"><a rel="callao/la-perla" href="<?php echo $ci_url; ?>ws/geomula/0/callao/callao/la-perla">La Perla</a></li> 
+				<li class="t"><a rel="callao/la-punta" href="<?php echo $ci_url; ?>ws/geomula/0/callao/callao/la-punta">La Punta</a></li> 
+				<li class="t"><a rel="callao/ventanilla" href="<?php echo $ci_url; ?>ws/geomula/0/callao/callao/ventanilla">Ventanilla</a></li> 
               </ul>                
 
               </li> 
@@ -22,17 +23,17 @@
               <li class="s"><a href="#">Lima Centro</a> 
 
               <ul>
-              	<li class="t"><a rel="lima/barranco" href="/shrekcms/mimula/ws/geomula/0/lima/lima/barranco">Barranco</a></li> 
-				<li class="t"><a rel="lima/breña" href="/shrekcms/mimula/ws/geomula/0/lima/lima/breña">Breña</a></li> 
-				<li class="t"><a rel="lima/jesús-maría" href="/shrekcms/mimula/ws/geomula/0/lima/lima/jesús-maría">Jesús María</a></li> 
-				<li class="t"><a rel="lima/la-victoria" href="/shrekcms/mimula/ws/geomula/0/lima/lima/la-victoria">La Victoria</a></li> 
-				<li class="t"><a rel="lima/lince" href="/shrekcms/mimula/ws/geomula/0/lima/lima/lince">Lince</a></li> 
-				<li class="t"><a rel="lima/miraflores" href="/shrekcms/mimula/ws/geomula/0/lima/lima/miraflores">Miraflores</a></li> 
-				<li class="t"><a rel="lima/san-isidro" href="/shrekcms/mimula/ws/geomula/0/lima/lima/san-isidro">San Isidro</a></li> 
-				<li class="t"><a rel="lima/san-luis" href="/shrekcms/mimula/ws/geomula/0/lima/lima/san-luis">San Luis</a></li>				
-				<li class="t"><a rel="lima/santiago-de-surco" href="/shrekcms/mimula/ws/geomula/0/lima/lima/santiago-de-surco">Santiago de Surco</a></li> 
-				<li class="t"><a rel="lima/surquillo" href="/shrekcms/mimula/ws/geomula/0/lima/lima/surquillo">Surquillo</a></li> 
-				<li class="t"><a rel="lima/san-borja" href="/shrekcms/mimula/ws/geomula/0/lima/lima/san-borja">San Borja</a></li>              
+              	<li class="t"><a rel="lima/barranco" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/barranco">Barranco</a></li> 
+				<li class="t"><a rel="lima/breña" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/breña">Breña</a></li> 
+				<li class="t"><a rel="lima/jesús-maría" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/jesús-maría">Jesús María</a></li> 
+				<li class="t"><a rel="lima/la-victoria" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/la-victoria">La Victoria</a></li> 
+				<li class="t"><a rel="lima/lince" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/lince">Lince</a></li> 
+				<li class="t"><a rel="lima/miraflores" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/miraflores">Miraflores</a></li> 
+				<li class="t"><a rel="lima/san-isidro" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/san-isidro">San Isidro</a></li> 
+				<li class="t"><a rel="lima/san-luis" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/san-luis">San Luis</a></li>				
+				<li class="t"><a rel="lima/santiago-de-surco" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/santiago-de-surco">Santiago de Surco</a></li> 
+				<li class="t"><a rel="lima/surquillo" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/surquillo">Surquillo</a></li> 
+				<li class="t"><a rel="lima/san-borja" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/san-borja">San Borja</a></li>              
               </ul>
 
               </li>
@@ -41,11 +42,11 @@
               <li class="s"><a href="#">Lima Oeste</a>
         
                 <ul>
-	              	<li class="t"><a rel="lima/cercado-de-lima" href="/shrekcms/mimula/ws/geomula/0/lima/lima7cercado-de-lima">Cercado de Lima</a></li> 
-					<li class="t"><a rel="lima/magdalena-del-mar" href="/shrekcms/mimula/ws/geomula/0/lima/lima7magdalena-del-mar">Magdalena del Mar</a></li> 
-					<li class="t"><a rel="lima/pueblo-libre" href="/shrekcms/mimula/ws/geomula/0/lima/lima7pueblo-libre">Pueblo Libre</a></li> 
+	              	<li class="t"><a rel="lima/cercado-de-lima" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima7cercado-de-lima">Cercado de Lima</a></li> 
+					<li class="t"><a rel="lima/magdalena-del-mar" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima7magdalena-del-mar">Magdalena del Mar</a></li> 
+					<li class="t"><a rel="lima/pueblo-libre" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima7pueblo-libre">Pueblo Libre</a></li> 
 					
-					<li class="t"><a rel="lima/san-miguel" href="/shrekcms/mimula/ws/geomula/0/lima/lima7san-miguel">San Miguel</a></li> 
+					<li class="t"><a rel="lima/san-miguel" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima7san-miguel">San Miguel</a></li> 
                 </ul>
           
               </li>                          
@@ -56,14 +57,14 @@
               
               <ul>
 
-              	<li class="t"><a rel="lima/chaclacayo" href="/shrekcms/mimula/ws/geomula/0/lima/lima/chaclacayo">Chaclacayo</a></li> 
-				<li class="t"><a rel="lima/cieneguilla" href="/shrekcms/mimula/ws/geomula/0/lima/lima/cieneguilla">Cieneguilla</a></li> 
-				<li class="t"><a rel="lima/el-agustino" href="/shrekcms/mimula/ws/geomula/0/lima/lima/el-agustino">El Agustino</a></li> 
-				<li class="t"><a rel="lima/la-molina" href="/shrekcms/mimula/ws/geomula/0/lima/lima/la-molina">La Molina</a></li> 
-				<li class="t"><a rel="lima/lurigancho" href="/shrekcms/mimula/ws/geomula/0/lima/lima/lurigancho">Lurigancho</a></li> 
+              	<li class="t"><a rel="lima/chaclacayo" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/chaclacayo">Chaclacayo</a></li> 
+				<li class="t"><a rel="lima/cieneguilla" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/cieneguilla">Cieneguilla</a></li> 
+				<li class="t"><a rel="lima/el-agustino" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/el-agustino">El Agustino</a></li> 
+				<li class="t"><a rel="lima/la-molina" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/la-molina">La Molina</a></li> 
+				<li class="t"><a rel="lima/lurigancho" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/lurigancho">Lurigancho</a></li> 
 				
-				<li class="t"><a rel="lima/san-juan-de-lurigancho" href="/shrekcms/mimula/ws/geomula/0/lima/lima/san-juan-de-lurigancho">San Juan de Lurigancho</a></li> 
-				<li class="t"><a rel="lima/santa-anita" href="/shrekcms/mimula/ws/geomula/0/lima/lima/santa-anita">Santa Anita</a></li> 
+				<li class="t"><a rel="lima/san-juan-de-lurigancho" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/san-juan-de-lurigancho">San Juan de Lurigancho</a></li> 
+				<li class="t"><a rel="lima/santa-anita" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/santa-anita">Santa Anita</a></li> 
      
               </ul>
 
@@ -73,14 +74,14 @@
         
                 <ul>
 
-	              	<li class="t"><a rel="lima/carabayllo" href="/shrekcms/mimula/ws/geomula/0/lima/lima/carabayllo">Carabayllo</a></li> 
-					<li class="t"><a rel="lima/comas" href="/shrekcms/mimula/ws/geomula/0/lima/lima/comas">Comas</a></li> 
+	              	<li class="t"><a rel="lima/carabayllo" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/carabayllo">Carabayllo</a></li> 
+					<li class="t"><a rel="lima/comas" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/comas">Comas</a></li> 
 					
-					<li class="t"><a rel="lima/independencia" href="/shrekcms/mimula/ws/geomula/0/lima/lima/independencia">Independencia</a></li> 
-					<li class="t"><a rel="lima/los-olivos" href="/shrekcms/mimula/ws/geomula/0/lima/lima/los-olivos">Los Olivos</a></li> 
-					<li class="t"><a rel="lima/puente-piedra" href="/shrekcms/mimula/ws/geomula/0/lima/lima/puente-piedra">Puente Piedra</a></li> 
-					<li class="t"><a rel="lima/rimac" href="/shrekcms/mimula/ws/geomula/0/lima/lima/rimac">Rimac</a></li> 
-					<li class="t"><a rel="lima/san-martin-de-porres" href="/shrekcms/mimula/ws/geomula/0/lima/lima/san-martin-de-porres">San Martin de Porres</a></li> 
+					<li class="t"><a rel="lima/independencia" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/independencia">Independencia</a></li> 
+					<li class="t"><a rel="lima/los-olivos" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/los-olivos">Los Olivos</a></li> 
+					<li class="t"><a rel="lima/puente-piedra" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/puente-piedra">Puente Piedra</a></li> 
+					<li class="t"><a rel="lima/rimac" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/rimac">Rimac</a></li> 
+					<li class="t"><a rel="lima/san-martin-de-porres" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/san-martin-de-porres">San Martin de Porres</a></li> 
 
                 </ul>
           
@@ -91,16 +92,16 @@
 
               <ul>
 
-              <li class="t"><a rel="lima/chorrillos" href="/shrekcms/mimula/ws/geomula/0/lima/lima/chorrillos">Chorrillos</a></li> 
-				<li class="t"><a rel="lima/lurian" href="/shrekcms/mimula/ws/geomula/0/lima/lima/lurian">Lurian</a></li> 
-				<li class="t"><a rel="lima/pachacamac" href="/shrekcms/mimula/ws/geomula/0/lima/lima/pachacamac">Pachacamac</a></li> 
-				<li class="t"><a rel="lima/pucusana" href="/shrekcms/mimula/ws/geomula/0/lima/lima/pucusana">Pucusana</a></li> 
-				<li class="t"><a rel="lima/san-bartolo" href="/shrekcms/mimula/ws/geomula/0/lima/lima/san-bartolo">San Bartolo</a></li> 
-				<li class="t"><a rel="lima/san-juan-de-miraflores" href="/shrekcms/mimula/ws/geomula/0/lima/lima/san-juan-de-miraflores">San Juan de Miraflores</a></li> 
-				<li class="t"><a rel="lima/sta.-maria-del-mar" href="/shrekcms/mimula/ws/geomula/0/lima/lima/sta.-maria-del-mar">Sta. Maria del Mar</a></li> 
-				<li class="t"><a rel="lima/villa-el-salvador" href="/shrekcms/mimula/ws/geomula/0/lima/lima/villa-el-salvador">Villa El Salvador</a></li> 
+              <li class="t"><a rel="lima/chorrillos" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/chorrillos">Chorrillos</a></li> 
+				<li class="t"><a rel="lima/lurian" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/lurian">Lurian</a></li> 
+				<li class="t"><a rel="lima/pachacamac" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/pachacamac">Pachacamac</a></li> 
+				<li class="t"><a rel="lima/pucusana" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/pucusana">Pucusana</a></li> 
+				<li class="t"><a rel="lima/san-bartolo" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/san-bartolo">San Bartolo</a></li> 
+				<li class="t"><a rel="lima/san-juan-de-miraflores" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/san-juan-de-miraflores">San Juan de Miraflores</a></li> 
+				<li class="t"><a rel="lima/sta.-maria-del-mar" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/sta.-maria-del-mar">Sta. Maria del Mar</a></li> 
+				<li class="t"><a rel="lima/villa-el-salvador" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/villa-el-salvador">Villa El Salvador</a></li> 
 				
-				<li class="t"><a rel="lima/villa-maria-del-triunfo" href="/shrekcms/mimula/ws/geomula/0/lima/lima/villa-maria-del-triunfo">Villa Maria del Triunfo</a></li> 
+				<li class="t"><a rel="lima/villa-maria-del-triunfo" href="<?php echo $ci_url; ?>ws/geomula/0/lima/lima/villa-maria-del-triunfo">Villa Maria del Triunfo</a></li> 
 				                
 
               </ul>
@@ -114,11 +115,11 @@
           <ul>
                 <li class="current s">Centro</li>
 
-              	<li class="s"><a rel="apurimac" href="/shrekcms/mimula/ws/geomula/0/apurimac">Apurimac</a></li> 
-				<li class="s"><a rel="huancavelica" href="/shrekcms/mimula/ws/geomula/0/huancavelica">Huancavelica</a></li> 
-				<li class="s"><a rel="huanuco" href="/shrekcms/mimula/ws/geomula/0/huanuco">Huanuco</a></li> 
-				<li class="s"><a rel="junin" href="/shrekcms/mimula/ws/geomula/0/junin">Junin</a></li> 
-				<li class="s"><a rel="pasco" href="/shrekcms/mimula/ws/geomula/0/pasco">Pasco</a></li> 
+              	<li class="s"><a rel="apurimac" href="<?php echo $ci_url; ?>ws/geomula/0/apurimac">Apurimac</a></li> 
+				<li class="s"><a rel="huancavelica" href="<?php echo $ci_url; ?>ws/geomula/0/huancavelica">Huancavelica</a></li> 
+				<li class="s"><a rel="huanuco" href="<?php echo $ci_url; ?>ws/geomula/0/huanuco">Huanuco</a></li> 
+				<li class="s"><a rel="junin" href="<?php echo $ci_url; ?>ws/geomula/0/junin">Junin</a></li> 
+				<li class="s"><a rel="pasco" href="<?php echo $ci_url; ?>ws/geomula/0/pasco">Pasco</a></li> 
           </ul>
         </li> <!-- centro -->
 
@@ -126,13 +127,13 @@
           <ul>
                 <li class="current s">Norte</li>
 
-              	<li class="s"><a rel="ancash" href="/shrekcms/mimula/ws/geomula/0/ancash">Ancash</a></li> 
-				<li class="s"><a rel="cajamarca" href="/shrekcms/mimula/ws/geomula/0/cajamarca">Cajamarca</a></li> 
-				<li class="s"><a rel="la-libertad" href="/shrekcms/mimula/ws/geomula/0/la-libertad">La Libertad</a></li> 
-				<li class="s"><a rel="lambayeque" href="/shrekcms/mimula/ws/geomula/0/lambayeque">Lambayeque</a></li> 
-				<li class="s"><a rel="piura" href="/shrekcms/mimula/ws/geomula/0/piura">Piura</a></li> 
+              	<li class="s"><a rel="ancash" href="<?php echo $ci_url; ?>ws/geomula/0/ancash">Ancash</a></li> 
+				<li class="s"><a rel="cajamarca" href="<?php echo $ci_url; ?>ws/geomula/0/cajamarca">Cajamarca</a></li> 
+				<li class="s"><a rel="la-libertad" href="<?php echo $ci_url; ?>ws/geomula/0/la-libertad">La Libertad</a></li> 
+				<li class="s"><a rel="lambayeque" href="<?php echo $ci_url; ?>ws/geomula/0/lambayeque">Lambayeque</a></li> 
+				<li class="s"><a rel="piura" href="<?php echo $ci_url; ?>ws/geomula/0/piura">Piura</a></li> 
 				
-				<li class="s"><a rel="tumbes" href="/shrekcms/mimula/ws/geomula/0/tumbes">Tumbes</a></li>\
+				<li class="s"><a rel="tumbes" href="<?php echo $ci_url; ?>ws/geomula/0/tumbes">Tumbes</a></li>\
            </ul>
         </li>  <!-- norte -->
 
@@ -140,12 +141,12 @@
             <ul>
                   <li class="current s">Oriente</li>
                     
-	              	<li class="s"><a rel="amazonas" href="/shrekcms/mimula/ws/geomula/0/amazonas">Amazonas</a></li> 
+	              	<li class="s"><a rel="amazonas" href="<?php echo $ci_url; ?>ws/geomula/0/amazonas">Amazonas</a></li> 
 
-					<li class="s"><a rel="loreto" href="/shrekcms/mimula/ws/geomula/0/loreto">Loreto</a></li> 
-					<li class="s"><a rel="madre-de-dios" href="/shrekcms/mimula/ws/geomula/0/madre-de-dios">Madre de Dios</a></li> 
-					<li class="s"><a rel="san-martin" href="/shrekcms/mimula/ws/geomula/0/san-martin">San Martin</a></li> 
-					<li class="s"><a rel="ucayali" href="/shrekcms/mimula/ws/geomula/0/ucayali">Ucayali</a></li> 
+					<li class="s"><a rel="loreto" href="<?php echo $ci_url; ?>ws/geomula/0/loreto">Loreto</a></li> 
+					<li class="s"><a rel="madre-de-dios" href="<?php echo $ci_url; ?>ws/geomula/0/madre-de-dios">Madre de Dios</a></li> 
+					<li class="s"><a rel="san-martin" href="<?php echo $ci_url; ?>ws/geomula/0/san-martin">San Martin</a></li> 
+					<li class="s"><a rel="ucayali" href="<?php echo $ci_url; ?>ws/geomula/0/ucayali">Ucayali</a></li> 
                
             </ul>
           </li>  <!-- oriente -->
@@ -154,13 +155,13 @@
           <ul>
                 <li class="current s">Sur</li>  
 
-              	<li class="s"><a rel="arequipa" href="/shrekcms/mimula/ws/geomula/0/arequipa">Arequipa</a></li> 
-				<li class="s"><a rel="ayacucho" href="/shrekcms/mimula/ws/geomula/0/ayacucho">Ayacucho</a></li> 
-				<li class="s"><a rel="cusco" href="/shrekcms/mimula/ws/geomula/0/cusco">Cusco</a></li> 
-				<li class="s"><a rel="ica" href="/shrekcms/mimula/ws/geomula/0/ica">Ica</a></li> 
-				<li class="s"><a rel="moquegua" href="/shrekcms/mimula/ws/geomula/0/moquegua">Moquegua</a></li> 
-				<li class="s"><a rel="puno" href="/shrekcms/mimula/ws/geomula/0/puno">Puno</a></li> 
-				<li class="s"><a rel="tacna" href="/shrekcms/mimula/ws/geomula/0/tacna">Tacna</a></li> 
+              	<li class="s"><a rel="arequipa" href="<?php echo $ci_url; ?>ws/geomula/0/arequipa">Arequipa</a></li> 
+				<li class="s"><a rel="ayacucho" href="<?php echo $ci_url; ?>ws/geomula/0/ayacucho">Ayacucho</a></li> 
+				<li class="s"><a rel="cusco" href="<?php echo $ci_url; ?>ws/geomula/0/cusco">Cusco</a></li> 
+				<li class="s"><a rel="ica" href="<?php echo $ci_url; ?>ws/geomula/0/ica">Ica</a></li> 
+				<li class="s"><a rel="moquegua" href="<?php echo $ci_url; ?>ws/geomula/0/moquegua">Moquegua</a></li> 
+				<li class="s"><a rel="puno" href="<?php echo $ci_url; ?>ws/geomula/0/puno">Puno</a></li> 
+				<li class="s"><a rel="tacna" href="<?php echo $ci_url; ?>ws/geomula/0/tacna">Tacna</a></li> 
                                                                                                                                                                                                            
           </ul>
         </li>  <!-- sur -->
