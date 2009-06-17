15fb508bb5e06dc98f13437b9cda6998746d119e
Juan Alberto Farias <dientuki@grimlock.(none)>
2009-06-17 02:30:13 -0500

    killed bug de la geomula, no recarga mas la pagina

--- wp/wp-content/themes/lamula/js/site.js
+++ wp/wp-content/themes/lamula/js/site.js
@@ -238,7 +238,7 @@ jQuery(document).ready(function($) {
 				$(this).next().children().show("fast");
 			}
 			
-			if (this.href != 'http://localhost/shrekcms/wp/#')
+			if (this.href != 'http://lamula.pe/mulapress/#')
 			{
 		   	      $.get(this.href, function(data){
 		   	    	  if (geomula_html == '')
