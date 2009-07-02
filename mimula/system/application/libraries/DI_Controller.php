e0cd3cbe9e84d9cde737d08f6d38eb2bd90308f9
Juan Alberto Farias <dientuki@grimlock.(none)>
2009-07-02 16:15:44 -0500

    documentado librerias adicionales

--- mimula/system/application/libraries/DI_Controller.php
+++ mimula/system/application/libraries/DI_Controller.php
@@ -1,32 +1,27 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 /**
- * CodeIgniter
  *
- * An open source application development framework for PHP 4.3.2 or newer
+ * Reescritura del controlador de Codeigniter
  *
- * @package		CodeIgniter
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2008, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
- * @filesource
+ * @package		mulapress
+ * @author		Srdperu | Juan Alberto
+ * @version		Version 1.0
  */
 
 // ------------------------------------------------------------------------
 
 /**
- * CodeIgniter Application Controller Class
+ * Reescritura del controlador de Codeigniter
  *
- * This class object is the super class the every library in
- * CodeIgniter will be assigned to.
  *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Libraries
- * @author		ExpressionEngine Dev Team
- * @link		http://codeigniter.com/user_guide/general/controllers.html
+ * @package		mulapress
+ * @subpackage	Controllers
+ * @category	Controllers
+ * @author		Srdperu | Juan Alberto
+ * @version		Version 1.0
  */
+
 class DI_Controller extends Controller {
 
     /**
--- mimula/system/application/libraries/Layout.php
+++ mimula/system/application/libraries/Layout.php
@@ -1,4 +1,26 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ *
+ * Libreria para el layout
+ *
+ * @package		mulapress
+ * @author		Srdperu | Juan Alberto
+ * @version		Version 1.0
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Libreria para el layout
+ *
+ *
+ * @package		mulapress
+ * @subpackage	Controllers
+ * @category	Controllers
+ * @author		Srdperu | Juan Alberto
+ * @version		Version 1.0
+ */
 
 class Layout {
 	
