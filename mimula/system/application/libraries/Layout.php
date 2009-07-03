83a9866edc49c5ac8c058f959b998d0b0e55c08f
Juan Alberto Farias <dientuki@grimlock.(none)>
2009-07-03 03:31:39 -0500

    documentado mas cosas

--- mimula/system/application/libraries/Layout.php
+++ mimula/system/application/libraries/Layout.php
@@ -16,23 +16,41 @@
  *
  *
  * @package		mulapress
- * @subpackage	Controllers
- * @category	Controllers
+ * @subpackage	Libraries
+ * @category	Libraries
  * @author		Srdperu | Juan Alberto
  * @version		Version 1.0
  */
 
 class Layout {
 	
+    /**
+     * CodeIgniter
+     * @var object
+     *
+     */		
 	var $CI = NULL;
+    /**
+     * subidon
+     * @var array
+     *
+     */		
 	var $subidon = NULL;
 	
+	/**
+	 * Constructor de la case
+	 */  	
 	function __construct()
 	{
 		$this->CI =& get_instance();
 		$this->subidon = array('fotos', 'documentos', 'articulos','videos','audios');
 	}
 	
+	/**
+	 * Agrega selected a un tag
+	 * @param string $target controlador actual  
+	 * @return string
+	 */  	
 	function active_controller($target)
 	{
 		if (in_array($this->CI->uri->segment(1), $this->subidon))
@@ -45,3 +63,6 @@ class Layout {
 		}		
 	}
 }
+
+/* End of file Layout.php */
+/* Location: ./system/application/libraries/Layout.php */
--- mimula/system/application/libraries/Wpfunctions.php
+++ mimula/system/application/libraries/Wpfunctions.php
@@ -1,7 +1,37 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ *
+ * Libreria para algunas funciones de wp
+ *
+ * @package		mulapress
+ * @author		Srdperu | Juan Alberto
+ * @version		Version 1.0
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Libreria para algunas funciones de wp
+ *
+ *
+ * @package		mulapress
+ * @subpackage	Libraries
+ * @category	Libraries
+ * @author		Srdperu | Juan Alberto
+ * @version		Version 1.0
+ */
+
 class Wpfunctions {
 	
+/**
+ * Serialize data, if needed.
+ *
+ * @since 2.0.5
+ *
+ * @param mixed $data Data that might be serialized.
+ * @return mixed A scalar data
+ */	
 	function maybe_serialize($data)
 	{
 		if ( is_array( $data ) || is_object( $data ) )
@@ -13,12 +43,34 @@ class Wpfunctions {
 		return $data;		
 	}
 	
+/**
+ * Navigates through an array and removes slashes from the values.
+ *
+ * If an array is passed, the array_map() function causes a callback to pass the
+ * value back to the function. The slashes from this value will removed.
+ *
+ * @since 2.0.0
+ *
+ * @param array|string $value The array or string to be striped.
+ * @return array|string Stripped array (or string in the callback).
+ */	
 	function stripslashes_deep($value)
 	{
 		$value = is_array($value) ? array_map($this->stripslashes_deep, $value) : stripslashes($value);
 		return $value;		
 	}
 	
+/**
+ * Check value to find if it was serialized.
+ *
+ * If $data is not an string, then returned value will always be false.
+ * Serialized data is always a string.
+ *
+ * @since 2.0.5
+ *
+ * @param mixed $data Value to check to see if was serialized.
+ * @return bool False if not serialized and true if it was.
+ */	
 	function is_serialized( $data ) {
 		// if it isn't a string, it isn't serialized
 		if ( !is_string( $data ) )
@@ -45,4 +97,6 @@ class Wpfunctions {
 		return false;
 	}
 }
-?>
+
+/* End of file Wpfunctions.php */
+/* Location: ./system/application/libraries/Wpfunctions.php */
