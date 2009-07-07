4d2ec52a4bed1c60ecd8d9da6da6f37f6e2e0dbe
Alvaro Pereyra Rabanal <alvaropereyrarabanal@ordenador-de-alvaro-pereyra-rabanal.local>
2009-07-07 17:40:59 -0500

    Changes from Doc

--- trunk/bp-themes/bpskeletonmember/profile/profile-menu.php
+++ trunk/bp-themes/bpskeletonmember/profile/profile-menu.php
@@ -34,12 +34,37 @@ $current_profile_id =  $bp->displayed_user->id;
 
 	$sql['select'] = 'SELECT puntaje, mularango';
 	$sql['from'] = 'FROM wp_users';
-	$sql['where'] = 'ID = \'' . $current_profile_id . '\''; 
+	$sql['where'] = 'ID = ' . $current_profile_id . ''; 
 
 	$ranking = $wpdb->get_results(implode(' ', $sql));
  ?>
 <div id="ranking_<?php echo preg_replace('/ /', '', $ranking->mularango); ?>">
-  <p><?php echo $ranking->mularango; ?> con <?php echo $ranking->puntaje; ?></p>
+  <!-- <p><?php echo $current_profile_id . $ranking->mularango; ?> con <?php echo $ranking->puntaje; ?></p> -->
 </div>
+
+<h3>Blogs</h3>
+
+<?php do_action( 'template_notices' ) ?>
+
+<?php if ( bp_has_blogs() ) : ?>
+	
+	<ul id="blog-list" class="item-list">
+		<?php while ( bp_blogs() ) : bp_the_blog(); ?>
+			
+			<li>
+				<h4><a href="<?php bp_blog_permalink() ?>" title="<?php bp_blog_title() ?>"><?php bp_blog_title() ?></a></h4>
+				<!-- <p><?php bp_blog_description() ?></p> -->
+			</li>
+			
+		<?php endwhile; ?>
+	</ul>
+	
+<?php else: ?>
+
+	<div id="message" class="info">
+		<p><?php bp_word_or_name( __( "You haven't created any blogs yet.", 'buddypress' ), __( "%s hasn't created any public blogs yet.", 'buddypress' ) ) ?> <?php bp_create_blog_link() ?></p>
+	</div>
+
+<?php endif;?>
 
 <?php bp_custom_profile_sidebar_boxes() ?>
